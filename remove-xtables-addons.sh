6f5334582573201c342d8bbb06f8915bece6cacd
blackfuel <blackfuel@hmamail.com>
2017-02-28 16:20:25 -0500

    new files and updated patches

--- remove-xtables-addons.sh
+++ remove-xtables-addons.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $HOME/blackfuel
-find asuswrt-merlin-xtables-addons-1.47.1 -type f -name "*" -printf "$HOME/blackfuel/asuswrt-merlin-blackfuel/%P\0" | xargs -0 -I '{}' rm '{}'
+find asuswrt-merlin-xtables-addons-1.47.1 -type f -name "*" -printf "$HOME/asuswrt-merlin/%P\0" | xargs -0 -I '{}' rm '{}'
 
