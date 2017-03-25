148fd989855894988daf38f49fa1aa58955b2eac
blackfuel <blackfuel@hmamail.com>
2017-03-24 21:59:49 -0400

    add rebuild-all switch

--- build-asuswrt-merlin-addon.sh
+++ build-asuswrt-merlin-addon.sh
@@ -2,13 +2,22 @@
 set -e
 set -x
 
-#REBUILD_ALL=0 CRYPTO_BACKEND="kernel" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
-REBUILD_ALL=0 CRYPTO_BACKEND="nettle" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
-#REBUILD_ALL=0 CRYPTO_BACKEND="openssl" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
-#REBUILD_ALL=0 CRYPTO_BACKEND="gcrypt" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
-REBUILD_ALL=0 ~/blackfuel/ntp-arm-asuswrt/ntp.sh
-REBUILD_ALL=0 ~/blackfuel/dnscrypt-arm-asuswrt/dnscrypt.sh
+[ -z "$REBUILD_ALL" ] && REBUILD_ALL=0
+
+#REBUILD_ALL=$REBUILD_ALL CRYPTO_BACKEND="kernel" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
+REBUILD_ALL=$REBUILD_ALL CRYPTO_BACKEND="nettle" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
+#REBUILD_ALL=$REBUILD_ALL CRYPTO_BACKEND="openssl" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
+#REBUILD_ALL=$REBUILD_ALL CRYPTO_BACKEND="gcrypt" ~/blackfuel/cryptsetup-arm-asuswrt/cryptsetup.sh
+REBUILD_ALL=$REBUILD_ALL ~/blackfuel/ntp-arm-asuswrt/ntp.sh
+REBUILD_ALL=$REBUILD_ALL ~/blackfuel/dnscrypt-arm-asuswrt/dnscrypt.sh
 
 # update the staging area
 rsync -avh --existing --delete-after $HOME/blackfuel/asuswrt-merlin-addon/asuswrt/* $HOME/blackfuel/asuswrt-merlin-addon/staging
 cp -a $HOME/blackfuel/asuswrt-merlin-addon/staging/native/sbin $HOME/blackfuel/asuswrt-merlin-addon/staging
+
+# strip binaries
+arm-brcm-linux-uclibcgnueabi-strip \
+  $HOME/blackfuel/asuswrt-merlin-addon/staging/bin/* \
+  $HOME/blackfuel/asuswrt-merlin-addon/staging/sbin/* \
+  $HOME/blackfuel/asuswrt-merlin-addon/staging/usr/bin/* \
+  $HOME/blackfuel/asuswrt-merlin-addon/staging/lib/*
