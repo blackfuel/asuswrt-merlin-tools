2cfa56b3ff35fc859c88843fc3274554b545f14a
blackfuel <blackfuel@github.com>
2018-08-25 14:56:16 -0400

    update patches and scripts

--- build-asuswrt-merlin.382.sh
+++ build-asuswrt-merlin.382.sh
@@ -52,7 +52,7 @@ make_clean_2() {
 }
 
 extract_source_code() {
-	tar xzvf /mnt/hgfs/sandbox/384.7-alpha1-43c4482.tar.gz
+	tar xzvf /mnt/hgfs/sandbox/384.7-alpha2-1a3674a.tar.gz
 }
 
 make_clean_3() {
